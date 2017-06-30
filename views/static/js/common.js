b6ba5be3855d88b3b3b9d0bf0f2919a4a4bdfb12
dan <729374940@qq.com>
2017-06-30 20:37:14 +0800

    登录页的bug已修复,2017年6月30日 20:36:36

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,9 +1,19 @@
 //公共头像,有返回值的尽可能的写在前面,cookie没有返回值,所以写在最后
 define(["jquery","template","cookie"],function($,template){
 	$(function(){
-		var userinfo = JSON.parse($.cookie('userinfo'));
-		var tplStr = template('prfile-tpl',userinfo);
-		$('#userinfo').html(tplStr);
+		//用户未登录,不能访问其他页面,都跳到登录页
+		//如果用户不在登录页的话
+		if("/dashboard/login"!=location.pathname){
+			//不在登录页,并且没有PHPSESSID,就跳转到登录页
+			if(!$.cookie("PHPSESSID")){
+				location.href='/dashboard/login';
+			}else{
+				var userinfo = JSON.parse($.cookie('userinfo'));
+				var tplStr = template('prfile-tpl',userinfo);
+				$('#userinfo').html(tplStr);
+			}
+		}
+		
 		//退出功能
 		$('#logout').click(function(){
 			$.ajax({
