d674dbf5eff4e232c3e28ea4b3dac8c7ebbbfb3e
dan <729374940@qq.com>
2017-07-07 16:09:52 +0800

    课时的添加和编辑,课时更新功能已完善2017年7月7日 16:09:50

--- views/static/js/course/steps3.js
+++ views/static/js/course/steps3.js
@@ -13,11 +13,13 @@ define(["jquery","template","util","ckeditor","form","bootstrap"],function($,tem
             $(".steps").html(html);
         }
     });
+    //点击跳到添加课程
     $(".steps").on("click","#lesson",function(){
         // alert(1);
         var html=template("modal-tpl",{
             title:"添加课时",
-            btnText:"添加"
+            btnText:"添加",
+            type:"add"
         });
         $(".modal-content").html(html);
 		$("#chapterModal").modal("show");
@@ -26,22 +28,18 @@ define(["jquery","template","util","ckeditor","form","bootstrap"],function($,tem
     $(".steps").on("click","#editor",function(){
         var ctid=$(this).data("id");
         //alert(ctid);
-        var isfree=0;
-        if($("#isfree").prop("checked")){
-            isfree=1;
-        }
         $.ajax({
             url:"/api/course/chapter/edit",
             type:"get",
             data:{
                 ct_id:ctid,
-                ct_is_free:isfree
             },
             success:function(data){
                 if(data.code==200){
-                    console.log(data);
+                    //console.log(data);
                     data.result.title="编辑课程";
                     data.result.btnText="保存";
+                    data.result.type="editor";
                     var html=template("modal-tpl",data.result);
                     $(".modal-content").html(html);
                     $("#chapterModal").modal("show");
@@ -51,5 +49,47 @@ define(["jquery","template","util","ckeditor","form","bootstrap"],function($,tem
         });
        
     })
+    //添加和编辑课程
+    $(".modal-content").on("click","#btnSave",function(){
+        //alert(1);
+        var type=$(this).data("type");
+        var isfree=0;
+        if($("#isfree").prop("checked")){
+            isfree=1;
+        }
+        var url="/api/course/chapter/add";
+        if(type=="editor"){
+            url="/api/course/chapter/modify";
+        }
+        $("form").ajaxSubmit({
+            type:"post",
+            url:url,
+            data:{
+                ct_is_free:isfree
+            },
+            success:function(data){
+                if(data.code==200){
+                    $("#chapterModal").modal("hide");
+                    //局部渲染课时
+                    //window.location.reload();
+                    $.ajax({
+                        url:"/api/course/lesson",
+                        type:"get",
+                        data:{
+                            cs_id:csid
+                        },
+                        success:function(data){
+                            if(data.code==200){
+                                //console.log(data);
+                                var html=template("lessonslist-tpl",data.result);
+                                $(".lessons>ul").html(html);
+                            }
+                        }
+                    })
+                }
+            }
+        });
+    })
+
             
 })
